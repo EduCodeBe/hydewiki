27e2787b2b2c91e602f0bad5a8a54b5996b8979c
Julien Pivotto <roidelapluie@inuits.eu>
2013-05-26 12:03:05 +0200

    initial commit

---
+++ hydewiki.py
@@ -0,0 +1,35 @@
+from jinja2 import nodes
+from jinja2.ext import Extension
+from mediawiki import wiki2html
+from jinja2 import environmentfilter
+
+
+@environmentfilter
+def mediawiki(env, value):
+    output = value
+    d = {}
+
+    return wiki2html(output, False)
+
+class MediawikiExtension(Extension):
+    # a set of names that trigger the extension.
+    tags = set(['mediawiki'])
+
+    def parse(self, parser):
+
+        lineno = parser.stream.next().lineno
+
+        body = parser.parse_statements(['name:endmediawiki'], drop_needle=True)
+
+        #args = [parser.parse_expression()]
+        return nodes.CallBlock(self.call_method('_render_mediawiki', None),
+                               [], [], body).set_lineno(lineno)
+
+    def _render_mediawiki(self, caller=None):
+        if not caller:
+            return ''
+        output = caller().strip()
+
+        return mediawiki(self.environment, output)
+
+
